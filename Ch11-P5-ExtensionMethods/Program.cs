b93d530565deb245976bf8d80cc019459839f675
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-10-14 23:30:47 +0500

    Project Created

---
+++ Ch11-P5-ExtensionMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11_P5_ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
